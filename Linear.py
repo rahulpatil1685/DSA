3515963e27a5f7af70c1e23f5e8db73cc656456e
rahulpatil1685 <rahulpatilrp5571085@gmail.com>
2026-08-04 12:39:36 +0530

    Add files via upload

---
+++ Linear.py
@@ -0,0 +1,14 @@
+n=int(input("Enter the number of customer: "))
+Id =[]
+for i in range (n):
+    id=int(input("Enter customer id: "))
+    Id.append(id)
+    
+key=int(input("Enter Account Id to Search: "))
+
+for i in range(len(Id)):
+    if Id[i] == key:
+        print("found at position: ",i+1);    
+        break
+else:
+    print("Not Found")
